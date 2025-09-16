86245d71e1b8b3d411c498bafb6b6dfa5f8a8148
JooseeSS <serrano.santos.jose.antonio@iescamas.es>
2025-09-16 20:42:36 +0200

    Add files via upload

---
+++ funcion.py
@@ -0,0 +1,15 @@
+def lower_num(num1, num2) :
+    if num1<=num2 :
+        lowest=num1
+    else:
+        lowest=num1
+    
+    return lowest
+
+first_num = int(input("introduce el primer numero"))
+second_num = int(input("introduce el segundo numero"))
+print("the lowest is " + str(lower_num(first_num, second_num)))
+
+
+
+  
