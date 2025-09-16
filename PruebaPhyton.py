11a5110c842426ffef6d4852365916797ea74949
JooseeSS <serrano.santos.jose.antonio@iescamas.es>
2025-09-16 20:22:06 +0200

    Add files via upload

---
+++ PruebaPhyton.py
@@ -0,0 +1,9 @@
+num1 = int(input("introduce el primer numero"))
+num2 = int(input("introduce el segundo numero"))
+
+if num1<=num2:
+    lowest=num1
+else:
+    lowest=num2
+    
+print("el numero mas pequeño es" +str(lowest))
